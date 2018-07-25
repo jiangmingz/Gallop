7853c68cfeaa95512a79d31b7fc95b53a211c18e
zhaojiangming@gmail.com <zhaojiangming@gmail.com>
2018-07-25 20:19:46 +0800

    修复gif图片访问错误

--- Demo/LWAsyncDisplayViewDemo/TableViewHeader.m
+++ Demo/LWAsyncDisplayViewDemo/TableViewHeader.m
@@ -1,8 +1,6 @@
 
 
 
-
-
 /********************* 有任何问题欢迎反馈给我 liuweiself@126.com ****************************************/
 /***************  https://github.com/waynezxcv/Gallop 持续更新 ***************************/
 /******************** 正在不断完善中，谢谢~  Enjoy ******************************************************/
@@ -15,7 +13,7 @@
 
 @interface TableViewHeader ()
 
-@property (nonatomic,strong) UIImageView* loadingView;
+@property(nonatomic, strong) UIImageView *loadingView;
 
 @end
 
@@ -25,28 +23,28 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor whiteColor];
-        
-        LWAsyncDisplayView* displayView =
-        [[LWAsyncDisplayView alloc] initWithFrame:CGRectMake(0.0f,- 100.0f,SCREEN_WIDTH,350.0f)];
+
+        LWAsyncDisplayView *displayView =
+                [[LWAsyncDisplayView alloc] initWithFrame:CGRectMake(0.0f, -100.0f, SCREEN_WIDTH, 350.0f)];
         [self addSubview:displayView];
         [self addSubview:self.loadingView];
-        
-        LWLayout* layout = [[LWLayout alloc] init];
-        LWImageStorage* bg = [[LWImageStorage alloc] init];
+
+        LWLayout *layout = [[LWLayout alloc] init];
+        LWImageStorage *bg = [[LWImageStorage alloc] init];
         bg.contents = [NSURL URLWithString:@"https://avatars0.githubusercontent.com/u/8408918?v=3&s=460"];
         bg.frame = CGRectMake(0.0f, 0.0f, SCREEN_WIDTH, displayView.bounds.size.height);
         bg.clipsToBounds = YES;
         [layout addStorage:bg];
-        
-        LWImageStorage* avtar = [[LWImageStorage alloc] init];
+
+        LWImageStorage *avtar = [[LWImageStorage alloc] init];
         avtar.contents = [NSURL URLWithString:@"https://avatars0.githubusercontent.com/u/8408918?v=3&s=460"];
         avtar.frame = CGRectMake(SCREEN_WIDTH - 90.0f, displayView.bounds.size.height - 40.0f, 80.0f, 80.0f);
         avtar.cornerRadius = 0.01f;
         avtar.cornerBorderColor = [UIColor whiteColor];
         avtar.cornerBorderWidth = 5.0f;
         [layout addStorage:avtar];
-        
-        
+
+
         displayView.layout = layout;
     }
     return self;
@@ -57,7 +55,7 @@
     if (_loadingView) {
         return _loadingView;
     }
-    _loadingView = [[UIImageView alloc] initWithFrame:CGRectMake(20.0f,-70.0f,25.0f,25.0f)];
+    _loadingView = [[UIImageView alloc] initWithFrame:CGRectMake(20.0f, -70.0f, 25.0f, 25.0f)];
     _loadingView.contentMode = UIViewContentModeScaleAspectFill;
     _loadingView.image = [UIImage imageNamed:@"loading"];
     _loadingView.clipsToBounds = YES;
@@ -66,18 +64,18 @@
 }
 
 - (void)loadingViewAnimateWithScrollViewContentOffset:(CGFloat)offset {
-    if (offset <= 0 && offset > - 200.0f) {
-        self.loadingView.transform = CGAffineTransformMakeRotation(offset* 0.1);
+    if (offset <= 0 && offset > -200.0f) {
+        self.loadingView.transform = CGAffineTransformMakeRotation(offset * 0.1f);
     }
 }
 
 - (void)refreshingAnimateBegin {
-    CABasicAnimation* rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
     rotationAnimation.duration = 0.5f;
     rotationAnimation.autoreverses = NO;
     rotationAnimation.repeatCount = HUGE_VALF;
-    rotationAnimation.fromValue = [NSNumber numberWithFloat:0.0f];
-    rotationAnimation.toValue = [NSNumber numberWithFloat:2 * M_PI];
+    rotationAnimation.fromValue = @0.0f;
+    rotationAnimation.toValue = @(2 * M_PI);
     [self.loadingView.layer addAnimation:rotationAnimation forKey:@"rotationAnimations"];
 }
 
